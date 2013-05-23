f892452adfdce0ce4ca4bf75d415f23047cc1974
Mike Linde <mike.linde@move.com>
2013-05-23 09:09:48 -0700

    Samba

---
+++ env_setup/samba.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Run as root
+ROOT_UID=0
+if [ "$UID" -ne "$ROOT_UID" ]; then
+  echo "Must be root to run this script."
+  exit
+fi
+
+# Verify arguments
+if [ "$#" -eq 1 ]; then
+  password=$1
+else
+  echo "Usage: $0 password"
+  exit
+fi
+
+mkdir /var/devroot
+chown movedev:movedev /var/devroot/
+chmod 775 /var/devroot/
+
+yum install samba samba-common -y
+
+printf "$password\n$password\n" | smbpasswd -a -s movedev
+
+content="[devroot]
+\npath=/var/devroot
+\nbrowseable=yes
+\nread only=no
+\ncomment=Development Root"
+echo -e $content >> smb.conf
+
+smbd reload
