2328d9a5d5d2628d8ad4d39cdce12b877c063b74
Mike Linde <mike.linde@move.com>
2013-05-27 12:04:30 -0700

    More

--- env_setup/rvm.sh
+++ env_setup/rvm.sh
@@ -8,7 +8,7 @@ if [ "$UID" -ne "$ROOT_UID" ]; then
 fi
 
 # Install SQLite, if necessary
-sudo yum install patch gcc-c++ readline-devel zlib-devel libyaml-devel libffi-devel openssl-devel autoconf automake libtool bison libxml2-devel libxslt-devel sqlite -y
+yum install patch gcc-c++ readline-devel zlib-devel libyaml-devel libffi-devel openssl-devel autoconf automake libtool bison libxml2-devel libxslt-devel sqlite -y
 
 # Install RVM
 curl -L https://get.rvm.io | bash -s stable 
